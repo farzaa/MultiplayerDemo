e9ef535b9a7ccac7864090c1112600624842dad3
Farzain Majeed <contactclothinghousenow@gmail.com>
2016-01-23 21:09:28 -0500

    Added server/connection with game

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,69 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	private Socket socket;
+
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		connectSocket();
+		configSocketEvents();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+
+	public void connectSocket() {
+		try {
+			socket = IO.socket("http://localhost:8080");
+			socket.connect();
+
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public void configSocketEvents() {
+		socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+					Gdx.app.log("SocketIO", "Connected");
+			}
+		}).on("socketID", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				System.out.print("Hi");
+				JSONObject data = (JSONObject) args[0];
+				try {
+					String id = data.getString("id");
+					Gdx.app.log("SocketIO", "My ID: " + id);
+				} catch(JSONException e) {
+					Gdx.app.log("SocketIO", "Error getting ID");
+				}
+			}
+		});
+	}
+}
